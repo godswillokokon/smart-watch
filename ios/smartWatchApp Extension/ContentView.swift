c91e5fb314359b0b22c7efa2ac15a773fe082525
Godswill <godswilll@analogrepublic.com>
2021-09-11 15:45:46 +0100

    watch app set

---
+++ ios/smartWatchApp Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  smartWatchApp Extension
+//
+//  Created by Godswill Okokon on 11/09/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios/smartWatchApp Extension/smartWatchApp.swift
@@ -0,0 +1,19 @@
+//
+//  smartWatchApp.swift
+//  smartWatchApp Extension
+//
+//  Created by Godswill Okokon on 11/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct smartWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
